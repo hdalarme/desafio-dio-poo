b7da3177e0270ca5d0b79cc5a9b74092c9daf2c9
Helbertt Dalarme <hdalarme@yahoo.com.br>
2023-05-31 09:36:33 -0300

    Primeiro commit

---
+++ src/main/java/xyz/helbertt/desafiodiopoo/DesafioDioPooApplication.java
@@ -0,0 +1,13 @@
+package xyz.helbertt.desafiodiopoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/helbertt/desafiodiopoo/DesafioDioPooApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.helbertt.desafiodiopoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
